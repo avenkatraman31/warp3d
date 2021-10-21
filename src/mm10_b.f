3a4ed4e0b45283848f5364c4896bf5fb36f047fa
Venkatraman, Aditya <adityav2018@gatech.edu>
2021-10-21 12:51:08 -0400

    twin_32

--- src/mm10_b.f
+++ src/mm10_b.f
@@ -1951,10 +1951,10 @@ c
         dt = np1%tinc
         m_alpha=props%cp_021
 c
-        if(n%twinned.eq.one .and. n%child.eq.one 
-     &    .and. dabs(tau).gt.tt(alpha)) then
-            tau=tt(alpha)*dsign(one,tau)
-        endif
+        ! if(n%twinned.eq.one .and. n%child.eq.one 
+      ! &    .and. dabs(tau).gt.tt(alpha)) then
+            ! tau=tt(alpha)*dsign(one,tau)
+        ! endif
 c ----------------------------------------------------------------------
 c                                                                      *
 c        Setting cp_001- gamma_dot_0^prism,
